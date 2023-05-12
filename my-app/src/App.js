adf1f469605017a59f9c37a2866cab8c419ac080
plip <plip2010195@yandex.ru>
2023-05-12 16:55:32 +0300

    add gameStat playerStat

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,12 @@
+import GameStat from "./components/GameStat/GameStat";
+import PlayersStat from "./components/PlayersStat/PlayersStat";
 
 function App() {
   return (
     <>
     Hello project
+    <PlayersStat />
+    <GameStat/>
     </>
   );
 }
---
+++ my-app/src/components/PlayersStat/PlayersStat.jsx
@@ -0,0 +1,5 @@
+export default function PlayersStat() {
+  return (
+    <div>PlayersStat</div>
+  )
+}
